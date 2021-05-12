904affd63e98655b1307748d59a4527165f8ba74
William McGuire <wmcguire18@gmail.com>
2021-05-12 14:34:11 -0400

    Added Iteration 1

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,44 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+return menuItemName = `Delicious ${name}`;
+};
 
+function createMenuItem(menuItemName, price, type) {
+return menuItem = {
+  name: menuItemName,
+  price: price,
+  type: type,
 }
+};
+
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item))
+    ingredients.push(item);
+  return ingredients;
+};
+
+function formatPrice(price) {
+return `$${price}`;
+};
+
+function decreasePrice(price) {
+var discount = price/10
+return price - discount;
+};
+
+function createRecipe(title, ingredients, type) {
+return recipe = {
+  title: title,
+  ingredients: ingredients,
+  type: type,
+}
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
