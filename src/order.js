4b9a1a9ccd32f53b343b9804aacfae149ad8117a
William McGuire <wmcguire18@gmail.com>
2021-05-12 19:31:24 -0400

    Added Iteration 2

--- src/order.js
+++ src/order.js
@@ -1,7 +1,43 @@
+function takeOrder(order, orderType){
+  if (orderType.length < 3){
+    orderType.push(order);
+  }
+};
+
+function refundOrder(orderNumber, orderType) {
+  for (i = 0; i < orderType.length; i++) {
+      if (orderType[i].orderNumber === orderNumber) {
+        orderType.splice(i, 1,);
+      }
+  }
+};
+
+function listItems(orderType) {
+  var orderList = []
+  for (i = 0; i < orderType.length; i++) {
+    orderList.push(orderType[i].item);
+  }
+  var finalOrderList = orderList.join(", ");
+  return finalOrderList;
+};
+
+function searchOrder(orderType, item) {
+  var orderList = [];
+for (i = 0; i < orderType.length; i++){
+  orderList.push(orderType[i].item);
+}
+if (orderList.includes(item)) {
+  return true;
+} else {
+  return false;
+}
+}
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
